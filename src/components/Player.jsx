99234b8d3da9b67c84790c55e0947ab9a067f942
Ali Tahvildari <36967683+alithv@users.noreply.github.com>
2024-09-26 18:58:47 +0200

    Adding some features of player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
   function handleEdit() {
-    setIsEditing(true);
+    setIsEditing(isEditing);
   }
 
   let playerName = <span className="player-name">{name}</span>;
   if (isEditing) {
-    playerName = <input type="text" required />;
+    playerName = <input type="text" required value={name} />;
   }
 
   return (
@@ -16,7 +16,7 @@ export default function Player({ name, symbol }) {
         {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEdit}>Edit</button>
+      <button onClick={handleEdit}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
