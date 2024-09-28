db2c22653e66c53f170e4561195d9f68d0d3de46
Ali Tahvildari <36967683+alithv@users.noreply.github.com>
2024-09-28 12:11:16 +0200

    Adding the feature of editing the name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,16 +1,23 @@
 import { useState } from "react";
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
+
   function handleEdit() {
     // You should never do this !isEditing which is new state value you should just pass a function to update the state
-    setIsEditing((editing) => {
-      return !editing;
-    });
+    setIsEditing((editing) => !editing);
   }
 
-  let playerName = <span className="player-name">{name}</span>;
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
   if (isEditing) {
-    playerName = <input type="text" required value={name} />;
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
   }
 
   return (
